5c1f36b05d5af994e5f682ef9fb78d50477a9d64
Tamzid Ahmed <tamjidahmed958@gmail.com>
2021-07-09 21:06:50 +0600

    splash screen time reduce

--- lib/screen/splash.dart
+++ lib/screen/splash.dart
@@ -16,7 +16,7 @@ class _SplashScreenState extends State<SplashScreen> {
   }
 
   Future<void> redirectPage() async => new Future.delayed(
-        const Duration(seconds: 40),
+        const Duration(seconds: 3),
         () => Navigator.pushReplacement(
           context,
           MaterialPageRoute(
